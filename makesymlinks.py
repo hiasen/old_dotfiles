6c4476126d4d27134999b9bd1b7afec6c86be494
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2013-10-14 21:33:57 +0200

    Made a script in python to automate symlink creation

---
+++ makesymlinks.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# Script for symlinking in dotfiles-repo
+# Øystein Hiåsen
+
+import os
+import datetime
+files_to_symlink = ['vimrc', 'gitconfig', 'tmux.conf']
+
+home_folder = os.path.expanduser('~') 
+dotfiles_repo = os.sep.join([home_folder, ".dotfiles"]) 
+source_path = os.sep.join([dotfiles_repo, '%s']) # Path to source of symlink
+symlink_path= os.sep.join([home_folder, ".%s"]) # path of symlink
+
+
+def symlink_dotfiles(files_to_symlink):
+    backup_folder_created = False
+    backup_folder = os.sep.join([home_folder, "dotfiles_backup_%s" % datetime.datetime.now().isoformat()])
+
+    for filename in files_to_symlink:
+        source = source_path % filename
+        symlink = symlink_path % filename
+
+        if os.path.exists(symlink):
+            if not backup_folder_created:
+                create_backup_folder(backup_folder)
+                backup_folder_created = True
+            print("Backing up old %s" % filename)
+            os.rename(symlink, os.path.sep.join([backup_folder,filename]))
+
+        print("Creating symlink for %s" % filename)
+        os.symlink(source , symlink)
+    print("Finished symlinking")
+
+
+def create_backup_folder(backup_folder):
+    print("Creating folder to backup existing dotfiles: %s" % backup_folder)
+    os.mkdir(backup_folder)
+    f = open(os.path.sep.join([backup_folder, 'README']), 'w')
+    text = "This folder was created by hiasens dotfiles symlinker."
+    f.write(text)
+    f.close()
+
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        symlink_dotfiles(sys.argv[1:])
+    else:
+        symlink_dotfiles(files_to_symlink)
+
